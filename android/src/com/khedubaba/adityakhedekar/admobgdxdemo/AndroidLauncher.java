58f87dc9002df981f9157c3a4a8cbf9e05f3d13f
khedubaba <adityakhedekar49@gmail.com>
2020-04-22 08:21:31 +0530

    Ad-Mob integration in lib-GDX project
    Signed-off-by: khedubaba <adityakhedekar49@gmail.com>

---
+++ android/src/com/khedubaba/adityakhedekar/admobgdxdemo/AndroidLauncher.java
@@ -0,0 +1,54 @@
+package com.khedubaba.adityakhedekar.admobgdxdemo;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
+import com.khedubaba.adityakhedekar.admobgdxdemo.AdMobGdxDemo;
+
+public class AndroidLauncher extends AndroidApplication {
+	private static final String TAG = "AndroidLauncher";
+	private RelativeLayout layout;
+	private RelativeLayout.LayoutParams params;
+	private AdView bannerAd;
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		View gameView = initializeForView(new AdMobGdxDemo(), config);
+
+		////////// Define the layout
+		layout = new RelativeLayout(this);
+		layout.addView(gameView, ViewGroup.LayoutParams.MATCH_PARENT,
+				ViewGroup.LayoutParams.MATCH_PARENT);
+
+		params = new RelativeLayout.LayoutParams(
+				ViewGroup.LayoutParams.WRAP_CONTENT,
+				ViewGroup.LayoutParams.WRAP_CONTENT);
+
+		params.addRule(RelativeLayout.ALIGN_PARENT_TOP);
+		params.addRule(RelativeLayout.CENTER_IN_PARENT);
+
+		bannerAd = new AdView(this);
+
+		//The ID below is a test ID: you need to add your Ad-Unit ID below
+		bannerAd.setAdUnitId("ca-app-pub-3940256099942544/6300978111");
+		bannerAd.setAdSize(AdSize.SMART_BANNER);
+
+		layout.addView(bannerAd, params);
+		setContentView(layout);
+
+		AdRequest ad = new AdRequest.Builder().build();
+		bannerAd.loadAd(ad);
+
+	}
+}
